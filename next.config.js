598a7d618d00a3621be53f3de948ab20790a8a2f
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2025-10-23 22:23:47 +0200

    Fix Prisma Vercel deployment with binary targets and server external packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	serverExternalPackages: ['@prisma/client', '@prisma/engines'],
 	images: {
 		remotePatterns: [
 			{ protocol: 'https', hostname: 'images.unsplash.com' },
